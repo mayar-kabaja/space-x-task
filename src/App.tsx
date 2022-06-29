48cbd5e9469c2c6c58f570d1eb26665b1db103d3
mayar-kabaja <kabajamayar@gmail.com>
2022-06-29 10:52:58 +0300

    set up react i18next translation Relates #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { i18n } = useTranslation();
+  const [lang] = useState(i18n.language);
+
+  useEffect(() => {
+    i18n.changeLanguage(lang);
+    document.dir = lang === 'en' ? 'ltr' : 'rtl';
+    document.documentElement.lang = lang;
+  }, [lang]);
   return <div className="App" />;
 }
 
